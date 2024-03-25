499661dccdd5ed3976b2ae74488e1ccc837c8ca9
imani <18imani.vata@gmail.com>
2024-03-25 12:37:12 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1= 31
+let num2= 2
+let multiply = num1*num2;
+console.log (multiply);
 
-// Write your code here
+let random= 5
+ Math.floor(Math.random() * 10) + 1;
+ (random)
+
+let num3= 16
+let num4= 6
+let mod= num3%num4 ;
+(mod)
+
+let max=20
+Math.max(14, 12, 3, 20);
+(max)
